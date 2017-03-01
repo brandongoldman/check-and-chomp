88986601d54258dfb75c768e26a967a3d5970245
amydevries <amy.devries@ufl.edu>
2017-02-28 20:50:55 -0500

    Map issue solved
    Got the map to center properly and add another marker for dining
    location

--- app.js
+++ app.js
@@ -1,70 +1,63 @@
-/*function initMap() {
-    console.log('initializing map'); 
-    var uf = {lat: 29.6436, lng: -82.3549};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 16,
-      center: uf
+var map;
+
+function initMap(theLat, theLong) {
+  // Try HTML5 geolocation.
+  console.log(theLat); 
+  console.log(theLong);
+  var im = 'http://www.robotwoods.com/dev/misc/bluecircle.png';
+  var bounds = new google.maps.LatLngBounds(); 
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var myLatLng = {
+      	lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      console.log(myLatLng);
+      
+      //var uf = {lat: 29.643362, lng: -82.354855};
+      var map = new google.maps.Map(document.getElementById('map'), {
+    		zoom: 14,
+    		center: myLatLng
+  	  });
+  	
+      var marker1 = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'My Location', 
+        icon: im
+      });
+      bounds.extend(marker1.position);
+      var location = {lat: theLat, lng: theLong};
+      var marker2 = new google.maps.Marker({
+        position: location,
+        map: map
+      });
+      bounds.extend(marker2.position); 
+      map.fitBounds(bounds); 
+     
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
     });
-}*/
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 
-function initMap() {
-        /*var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 29.6436, lng: -82.3549},
-          zoom: 10
-        });
-        var infoWindow = new google.maps.InfoWindow({map: map});*/
+}
 
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            /*var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };*/
-            var myLatLng = {
-            	lat: position.coords.latitude,
-              	lng: position.coords.longitude
-            };
-            var map = new google.maps.Map(document.getElementById('map'), {
-          		zoom: 10,
-          		center: myLatLng
-        	});
-        	map.setCenter(myLatLng);
-            var marker = new google.maps.Marker({
-	          position: myLatLng,
-	          map: map,
-	          title: 'My Location'
-	        });
-           
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+}
 
-            /*infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            map.setCenter(pos);*/
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-      }
 
 $('.tableRows').click(function(){
 	$(this).closest('tr').next('.expandedInformation').toggle();
-	resize(); 
+  initMap(29.647109, -82.341467); 
 });
-
-function resize(){
-	console.log('resizing map'); 	
-    google.maps.event.trigger(map, 'resize');
-}
 
 //source: https://developers.google.com/maps/documentation/javascript/examples/marker-simple
 //source: https://developers.google.com/maps/documentation/javascript/geolocation
