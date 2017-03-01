597f537d1d4e9564ca206b33a46f023975bc2d39
amydevries <amy.devries@ufl.edu>
2017-03-01 15:00:46 -0500

    Working on getting multiple maps to appear

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function sendLocation(position){
   initMilesAway(myLatLng);
 }
 
-function initMap(theLat, theLong) {
+function initMap(theLat, theLong, index) {
   // Try HTML5 geolocation.
   var im = 'http://www.robotwoods.com/dev/misc/bluecircle.png';
   var bounds = new google.maps.LatLngBounds(); 
@@ -67,7 +67,7 @@ function initMap(theLat, theLong) {
       console.log(myLatLng);
 
       
-      var map = new google.maps.Map(document.getElementById('map'), {
+      var map = new google.maps.Map(document.getElementById('map' + index), {
     		zoom: 14,
     		center: myLatLng
   	  });
@@ -109,7 +109,8 @@ $.getJSON("regularHours.json", function(data) {
   var index = 0;
   $.each(data, function (key, val) {
     console.log(val._id);
-    var content1 = "<tr class=\"tableRows\"> <td class=\"col-md-4\" id=\"" + val._id + "\"><img src=\"" + val.logoLocation + "\" alt=\"" + val._id + " Logo\" class=\"logos\"><p class=\"miles\" id=\"theMiles\">0.8 miles</p></td> <td class=\"locationPreview\"> <h3 class=\"locationName\">" + val._id + "</h3> <h4 class=\"openTill\">Open till 7pm</h4> <center><i class=\"fa fa-chevron-down\" aria-hidden=\"true\"></i></center> </td> </tr> <tr class=\"expandedInformation\"> <td class=\"col-md-4 weeklyHours\"> <h5 class=\"hoursHeading\">Weekly Hours</h5><h6 id=\"dynamicHoursLoad" + index + "\"></h6> </td> <td class=\"displayMap\" id=\"map\"> </td> </tr>";    $("#dynamicRowLoad").append(content1);
+    var content1 = "<tr class=\"tableRows\"> <td class=\"col-md-4\" id=\"" + val._id + "\"><img src=\"" + val.logoLocation + "\" alt=\"" + val._id + " Logo\" class=\"logos\"><p class=\"miles\" id=\"theMiles\">0.8 miles</p></td> <td class=\"locationPreview\"> <h3 class=\"locationName\">" + val._id + "</h3> <h4 class=\"openTill\">Open till 7pm</h4> <center><i class=\"fa fa-chevron-down\" aria-hidden=\"true\"></i></center> </td> </tr> <tr class=\"expandedInformation\"> <td class=\"col-md-4 weeklyHours\"> <h5 class=\"hoursHeading\">Weekly Hours</h5><h6 id=\"dynamicHoursLoad" + index + "\"></h6> </td> <td class=\"displayMap\" id=\"map" + index + "\"> </td> </tr>";    
+    $("#dynamicRowLoad").append(content1);
     var content2 = "Monday: " + val.Monday + "<br>Tuesday: " + val.Tuesday + "<br>Wednesday: " + val.Wednesday + "<br>Thursday: " + val.Thursday + "<br>Friday: " + val.Friday + "<br>Saturday: " + val.Saturday + "<br>Sunday: " + val.Sunday;
     $("#dynamicHoursLoad" + index).html(content2);
     console.log(content2);
@@ -119,7 +120,8 @@ $.getJSON("regularHours.json", function(data) {
 
 $('#dynamicRowLoad').on('click', '.tableRows', function() {
  $(this).closest('tr').next('.expandedInformation').toggle();
- initMap(29.647109, -82.341467); 
+ console.log($(this).closest('tr').next('.expandedInformation'));
+ initMap(29.647109, -82.341467, 1); 
 });
 
 //source: https://developers.google.com/maps/documentation/javascript/examples/marker-simple
