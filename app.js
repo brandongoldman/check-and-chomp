0d936ee30ad5d8a3b10e0188bc0311b58463fe4a
amydevries <amy.devries@ufl.edu>
2017-03-01 21:11:23 -0500

    Displays the open till information now

--- app.js
+++ app.js
@@ -107,9 +107,101 @@ function loadTheTable(){
   $.getJSON("regularHours.json", function(data) {
     var index = 0;
     $.each(data, function (key, val) {
-      console.log(val._id);
-      var content1 = "<tr class=\"tableRows\"> <td class=\"col-md-4\" id=\"" + val._id + "\"><img src=\"" + val.logoLocation + "\" alt=\"" + val._id + " Logo\" class=\"logos\"><p class=\"miles\" id=\"theMiles" + index + "\"></p></td> <td class=\"locationPreview\"> <h3 class=\"locationName\">" + val._id + "</h3> <h4 class=\"openTill\">Open till 7pm</h4> <center><i class=\"fa fa-chevron-down\" aria-hidden=\"true\"></i></center> </td> </tr> <tr class=\"expandedInformation\"> <td class=\"col-md-4 weeklyHours\"> <h5 class=\"hoursHeading\">Weekly Hours</h5><h6 id=\"dynamicHoursLoad" + index + "\"></h6> </td> <td class=\"displayMap\" id=\"map" + index + "\"> </td> </tr>";    
-      $("#dynamicRowLoad").append(content1);
+      var content1 = "<tr class=\"tableRows\"> <td class=\"col-md-4\" id=\"" + val._id + "\"><img src=\"" + val.logoLocation + "\" alt=\"" + val._id + " Logo\" class=\"logos\"><p class=\"miles\" id=\"theMiles" + index + "\"></p></td> <td class=\"locationPreview\"> <h3 class=\"locationName\">" + val._id + "</h3> <h4 class=\"openTill\" id=\"openTill" + index + "\"></h4> <center><i class=\"fa fa-chevron-down\" aria-hidden=\"true\"></i></center> </td> </tr> <tr class=\"expandedInformation\"> <td class=\"col-md-4 weeklyHours\"> <h5 class=\"hoursHeading\">Weekly Hours</h5><h6 id=\"dynamicHoursLoad" + index + "\"></h6> </td> <td class=\"displayMap\" id=\"map" + index + "\"> </td> </tr>";  
+      $("#dynamicRowLoad").append(content1);  
+      getDateTimeInfo(); 
+      var dateString; 
+      switch(day){
+        case 0: dateString = val.Sunday; break;
+        case 1: dateString = val.Monday; break;
+        case 2: dateString = val.Tuesday; break;
+        case 3: dateString = val.Wednesday; break;
+        case 4: dateString = val.Thursday; break;
+        case 5: dateString = val.Friday; break;
+        case 6: dateString = val.Saturday; break;
+      }
+      console.log(dateString);
+      if(dateString === "CLOSED"){
+        document.getElementById('openTill' + index).innerHTML = "Closed";
+        //add code here to make the listing greyed out/disabled
+      }
+      else if(dateString === "24 HOURS"){
+        document.getElementById('openTill' + index).innerHTML = "24 Hours";
+      }
+      else{
+        //need to further parse the dateString
+        var locClosAmPm; 
+        var displayLocClosing;
+        var locClosHr; 
+        var locClosMin; 
+        var locOpenHr;
+        var locOpenMin; 
+        var splitForTime = dateString.split("&#45; ");
+        displayLocClosing = splitForTime[1];
+        var splitStringArray = dateString.split(" ");
+        locClosAmPm = splitStringArray[4];
+        locClosHr = splitStringArray[3];
+        var getHour = locClosHr.split(":");
+        locClosHr = getHour[0];
+        locClosMin = getHour[1];
+        locOpenHr = splitStringArray[0];
+        var getOpen = locOpenHr.split(":");
+        locOpenHr = getOpen[0];
+        locOpenMin = getOpen[1];
+        if(amPm == "PM" && locClosAmPm == "AM"){
+          document.getElementById('openTill' + index).innerHTML = 'Open till ' + displayLocClosing;
+        }
+        else if(amPm == "PM" && locClosAmPm == "PM"){
+          if(theHours < locClosHr){
+            document.getElementById('openTill' + index).innerHTML = 'Open till ' + displayLocClosing;
+          }
+          else if(theHours == locClosHr){
+            if(minutes < locClosMin){
+              document.getElementById('openTill' + index).innerHTML = 'Open till ' + displayLocClosing;
+            }
+            else{
+              document.getElementById('openTill' + index).innerHTML = "Closed";
+            }
+          }
+          else{
+            document.getElementById('openTill' + index).innerHTML = "Closed";
+          }
+        }
+        else if(amPm == "AM" && locClosAmPm == "AM"){
+          if(theHours < locClosHr){
+            document.getElementById('openTill' + index).innerHTML = 'Open till ' + displayLocClosing;
+          }
+          else if(theHours == locClosHr){
+            if(minutes < locClosMin){
+              document.getElementById('openTill' + index).innerHTML = 'Open till ' + displayLocClosing;
+            }
+            else{
+              document.getElementById('openTill' + index).innerHTML = "Closed";
+            }
+          }
+          else{
+            document.getElementById('openTill' + index).innerHTML = "Closed";
+          }
+        }
+        else if(amPm == "AM" && locClosAmPm == "PM"){
+          if(theHours < locOpenHr){
+            document.getElementById('openTill' + index).innerHTML = "Closed";
+          }
+          else if(theHours == locOpenHr){
+            if(locOpenMin > minutes){
+              document.getElementById('openTill' + index).innerHTML = "Closed";
+            }
+            else{
+              document.getElementById('openTill' + index).innerHTML = 'Open till ' + displayLocClosing;
+            }
+          }
+          else{
+            document.getElementById('openTill' + index).innerHTML = 'Open till ' + displayLocClosing;
+          }
+        }
+      }
+
+      
       var content2 = "Monday: " + val.Monday + "<br>Tuesday: " + val.Tuesday + "<br>Wednesday: " + val.Wednesday + "<br>Thursday: " + val.Thursday + "<br>Friday: " + val.Friday + "<br>Saturday: " + val.Saturday + "<br>Sunday: " + val.Sunday;
       $("#dynamicHoursLoad" + index).html(content2);
       //pass the coordinates to the function that calculates how far the user is from the location
@@ -136,6 +228,40 @@ $('#dynamicRowLoad').on('click', '.tableRows', function() {
     });
   });
 });
+
+var day; 
+var amPm; 
+var theHours; 
+var minutes; 
+
+function getDateTimeInfo(){
+  var currentDate = new Date(); 
+  day = currentDate.getDay();
+  var hours = currentDate.getHours();
+  if(hours < 12){
+    amPm = "AM";
+  } 
+  else{
+    amPm = "PM"
+  }
+  switch(hours){
+    case 0: theHours = 12; break; 
+    case 13: theHours = 1; break; 
+    case 14: theHours = 2; break; 
+    case 15: theHours = 3; break; 
+    case 16: theHours = 4; break; 
+    case 17: theHours = 5; break; 
+    case 18: theHours = 6; break; 
+    case 19: theHours = 7; break; 
+    case 20: theHours = 8; break; 
+    case 21: theHours = 9; break; 
+    case 22: theHours = 10; break; 
+    case 23: theHours = 11; break; 
+    theHours = hours; 
+  }
+  minutes = currentDate.getMinutes(); 
+}
+
 
 //source: https://developers.google.com/maps/documentation/javascript/examples/marker-simple
 //source: https://developers.google.com/maps/documentation/javascript/geolocation
